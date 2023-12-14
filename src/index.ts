3dcff0e8e8ce87c5e06e8caea53d593e883e1615
Aaron Dalton <aaron@daltons.ca>
2023-12-14 10:48:58 -0700

    Added Trueskill rater

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { IRaterOptions, IRaterResults, IRating, IELOOptions, ELOBasic, IGlickoOptions, IGlickoRating, Glicko2 } from "./raters";
+import { IRaterOptions, IRaterResults, IRating, IELOOptions, ELOBasic, IGlickoOptions, IGlickoRating, Glicko2, ITrueskillOptions, ITrueskillRating, Trueskill } from "./raters";
 import { APGameRecord } from "./schemas/gamerecord";
 
-export { IRaterOptions, IRaterResults, IRating, IELOOptions, ELOBasic, APGameRecord, IGlickoOptions, IGlickoRating, Glicko2 };
+export { IRaterOptions, IRaterResults, IRating, IELOOptions, ELOBasic, APGameRecord, IGlickoOptions, IGlickoRating, Glicko2, ITrueskillOptions, ITrueskillRating, Trueskill };
